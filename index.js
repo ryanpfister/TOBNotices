c70eceb6400bdef5e3fcff83c05f6c420b80fff3
Pfister <ryan.pfister.ext@siemens.com>
2024-06-25 10:32:17 -0400

    fixed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
